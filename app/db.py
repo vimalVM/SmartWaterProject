d969c9c1198989d9ff581e5eef24ff5f85a072c9
shettivaibhav <vaibhavgshetti@gmail>
2026-01-25 23:36:53 +0530

    databse update

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="#Vimal2004",
+        password="root",
         database="smart_water"
     )
